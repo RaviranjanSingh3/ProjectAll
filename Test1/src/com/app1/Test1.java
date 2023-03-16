4f12ef08c53843fc22271312ab01c5f70f76dc46
Ajay <rrs65291@gmail.com>
2023-03-17 00:30:47 +0530

    jdbjhbdbkd

--- Test1/src/com/app1/Test1.java
+++ Test1/src/com/app1/Test1.java
@@ -5,8 +5,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println("my name is ravi hjdfhbhhssvhhjvhgv");
-	int a =10;
-//bhfbbfbfhbbhh
   //jdbjsbh,,,sssswsxsdsds
 	int a =12;
 //int b =905;
